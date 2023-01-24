46f29536af039cd2c9e0e451a82c0f5d53ed551a
Joseph Madsen <josephmads@protonmail.com>
2023-01-23 23:01:52 -0600

    fix: paginates skills, materials, work type views for users and ideas

--- directory/views.py
+++ directory/views.py
@@ -57,13 +57,11 @@ def user_list_view(request):
         context = {'error': str(err)}
         return render(request, 'error_page.html', context, content_type='text/html')
 
-def user_detail_view(request, username):
+def user_detail_view(request, id):
     """View function diplays details of user."""
     try: 
-        user = User.objects.filter(username=username).first()
-        profile = Profile.objects.filter(user_id__username=username).first()
+        profile = Profile.objects.filter(user_id__id=id).first()
         context = {
-            'user': user,
             'profile': profile,
         }
         return render(request, 'directory/user_detail.html', context)
@@ -78,7 +76,12 @@ def list_skills(request, skill_id):
     """View function lists users by the skills they are tagged with."""
     try:
         skill = get_object_or_404(Skill, id=skill_id)
-        users = skill.profile.all()
+        all_users = skill.profile.all()
+        paginator = Paginator(all_users, 5)
+        page_number = request.GET.get('page')
+
+        users = paginator.get_page(page_number)
+
         context = {
             'skill_name': skill.skill,
             'users': users,
@@ -95,7 +98,12 @@ def list_materials(request, material_id):
     """View function lists users by the materials they are tagged with."""
     try: 
         material = get_object_or_404(Material, id=material_id)
-        users = material.profile.all()
+        all_users = material.profile.all()
+        paginator = Paginator(all_users, 5)
+        page_number = request.GET.get('page')
+
+        users = paginator.get_page(page_number)
+
         context = {
             'material_name': material.material,
             'users': users
@@ -112,7 +120,12 @@ def list_work_type(request, type_id):
     """View function lists users by the type of work they are tagged with."""
     try:
         type = get_object_or_404(WorkType, id=type_id)
-        users = type.profile.all()
+        all_users = type.profile.all()
+        paginator = Paginator(all_users, 5)
+        page_number = request.GET.get('page')
+
+        users = paginator.get_page(page_number)
+
         context = {
             'type_name': type.work_type,
             'users': users
@@ -167,10 +180,15 @@ def list_idea_skills(request, skill_id):
     """View function lists ideas by the skills they are tagged with."""
     try:
         skill = get_object_or_404(Skill, id=skill_id)
-        ideas = skill.idea.all()
+        all_ideas = skill.idea.all()
+        paginator = Paginator(all_ideas, 5)
+        page_number = request.GET.get('page')
+
+        idea_list = paginator.get_page(page_number)
+        
         context = {
             'skill_name': skill.skill,
-            'ideas': ideas,
+            'idea_list': idea_list,
         }
         return render(request, 'directory/idea_list.html', context)
     
@@ -183,10 +201,15 @@ def list_idea_materials(request, material_id):
     """View function lists ideas by the materials they are tagged with."""
     try:
         material = get_object_or_404(Material, id=material_id)
-        ideas = material.idea.all()
+        all_ideas = material.idea.all()
+        paginator = Paginator(all_ideas, 5)
+        page_number = request.GET.get('page')
+
+        idea_list = paginator.get_page(page_number)
+
         context = {
             'material_name': material.material,
-            'ideas': ideas
+            'idea_list': idea_list
         }
         return render(request, 'directory/idea_list.html', context)
     
@@ -200,10 +223,15 @@ def list_idea_work_type(request, type_id):
     """View function lists ideas by the type of work they are tagged with."""
     try:
         type = get_object_or_404(WorkType, id=type_id)
-        ideas = type.idea.all()
+        all_ideas = type.idea.all()
+        paginator = Paginator(all_ideas, 5)
+        page_number = request.GET.get('page')
+
+        idea_list = paginator.get_page(page_number)
+
         context = {
             'type_name': type.work_type,
-            'ideas': ideas
+            'idea_list': idea_list
         }
         return render(request, 'directory/idea_list.html', context)
     
