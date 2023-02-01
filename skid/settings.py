24138dfbbdb74cb9f4ad08640f798448f0ab225f
Joseph Madsen <josephmads@protonmail.com>
2023-02-01 13:05:58 +0000

    fix: error with username where id was needed

--- skid/settings.py
+++ skid/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['3.17.165.91', 'localhost']
+ALLOWED_HOSTS = ['sk-id.info', 'localhost']
 
 
 # Application definition
