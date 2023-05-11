b4bdabfd134a7b70d235c7a7f7a0a59665f01d73
Joseph Madsen <josephmads@protonmail.com>
2023-05-10 23:23:54 -0500

    adds selenium integration  tests

---
+++ users/tests/test_integration.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+
+class SeleniumTestCase(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = webdriver.ChromeOptions()
+        options.add_argument('--window-size=1440,1600')
+        cls.driver = webdriver.Chrome(options=options)
+        cls.driver.implicitly_wait(10)
+
+        User.objects.create_user(
+            username='testertim',
+            email='tim.timons@test.test',
+            first_name='Tim',
+            last_name='Timons',
+            password='001234567',
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+
+class UserForms(SeleniumTestCase):
+
+    def test_user_login_and_logout_form(self):
+        self.driver.get(self.live_server_url + '/accounts/login/')
+        user_name = self.driver.find_element(By.ID, 'id_login')
+        user_password = self.driver.find_element(By.ID, 'id_password')
+        submit = self.driver.find_element(By.ID, 'id_login_submit')
+
+        user_name.send_keys('testertim')
+        user_password.send_keys('001234567')
+        submit.click()
+        
+        messages = self.driver.find_element(By.CLASS_NAME, 'messages')
+        self.assertEqual(messages.text, 'Successfully signed in as testertim.')
+
+        logout_nav_button = self.driver.find_element(By.ID, 'id_nav_logout')
+        logout_nav_button.click()
+        self.driver.implicitly_wait(5)
+
+        logout_button = self.driver.find_element(By.ID, 'id_logout_submit')
+        logout_button.click()
+
+        messages = self.driver.find_element(By.CLASS_NAME, 'messages')
+        self.assertEqual(messages.text, 'You have signed out.')
