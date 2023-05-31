1543ae2d3121ea666009d1e7e02781af215c5978
Joseph Madsen <josephmads@protonmail.com>
2023-05-30 21:59:52 -0500

    closes new window and reloads parent

--- users/static/js/new_window.js
+++ users/static/js/new_window.js
@@ -1,9 +1,13 @@
+
 function openSkillWindow() {
-    // Open a new window with a specified URL
+    // Open a new window for user to add more skills
     let params = `width=800, height=400, menubar=no, toolbar=no, left=100, top=100`
 
-    window.open('/users/add-skill', 
-                'Add Skills', 
-                params
-                );
+    skillWindow = window.open('/users/add-skill', 'Add Skills', params);
+  }
+
+function closeWindow() {
+    // Closes new window, and reloads profile page
+    window.close();
+    window.opener.location.reload();
   }
